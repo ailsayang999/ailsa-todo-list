f1e8976150fde1c0561cb5528bc625f4f8ca5b67
AilsaYang <ailsayang831031@gmail.com>
2023-08-03 15:50:03 +0800

    feat: Add login api function and show info alert

---
+++ src/api/auth.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+//定義串接的 end point 網址
+const authURL = 'https://todo-list.alphacamp.io/api/auth';
+
+export const login = async ({ username, password }) => {
+  try {
+    //他會回傳我們一組auth token，因為axios的關係，回傳的response都會被封裝在一個data的物件裡面，我們用解構的方式取到data值
+    const { data } = await axios.post(`${authURL}/login`, {
+      username,
+      password,
+    });
+
+    //我們可以在這裡console.log一下看後端傳來的data有什麼
+    console.log(data)
+
+    //我們用解構的方式取到data裡面authToken的值
+    const { authToken } = data;
+    //如果authToken為true，則代表登入成功
+    if (authToken) {
+      return { success: true, ...data }; //在把後端傳來的成功data回傳資料給頁面做使用時，一併整理資料格式，加上 success 屬性做為 flag，之後就能用 success 屬性來判斷是否登入成功。這個data裡面有authToken會回傳出去
+    }
+
+    //登入失敗的話，我也必須讓我的頁面知道，並把後段的response data(這裡會是fail的data)給回傳給頁面
+    return data;
+  } catch (error) {
+    console.error("[Login Failed]:",error);
+  }
+};
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,56 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from 'api/auth';
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
-  const [userName, setUserName] = useState('');
+  const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+
+  //handle點擊登入按鈕時的event，在點擊登入的button時會去呼叫handleClick這個function，handleClick這個function會再去呼叫auth.js裡面的login非同步function
+  const handleClick = async () => {
+    //防止使用者沒有輸入username和password
+    if (username.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+    try {
+      //用解構去把success和authToken的值從return回來的data中取出來
+      const { success, authToken } = await login({
+        username,
+        password,
+      });
+
+      //登入成功
+      if (success) {
+        //通常我們會把我們的authToken存在localStorage
+        localStorage.setItem('authToken', authToken);
+        // 登入成功訊息
+        Swal.fire({
+          position: 'top',
+          title: '登入成功！',
+          timer: 1000,
+          icon: 'success',
+          showConfirmButton: false,
+        });
+        return;
+      }
+
+      // 登入失敗訊息
+      Swal.fire({
+        position: 'top',
+        title: '登入失敗！',
+        timer: 1000,
+        icon: 'error',
+        showConfirmButton: false,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <AuthContainer>
@@ -24,7 +70,7 @@ const LoginPage = () => {
         <AuthInput
           label={'帳號'}
           placeholder={'請輸入帳號'}
-          value={userName}
+          value={username}
           onChange={(nameInputValue) => setUserName(nameInputValue)}
         />
       </AuthInputContainer>
@@ -39,7 +85,7 @@ const LoginPage = () => {
         />
       </AuthInputContainer>
 
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <Link to="/signup">
         <AuthLinkText>註冊</AuthLinkText>
       </Link>
