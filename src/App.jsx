a0a6d18604691ce1750638430d78e12bbca4cdf0
AilsaYang <ailsayang831031@gmail.com>
2023-08-05 11:17:13 +0800

    feat: Add router basename

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 
 import { AuthProvider } from 'contexts/AuthContext';
 
+const basename = process.env.PUBLIC_URL
+
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <Routes>
             <Route path="login" element={<LoginPage />} />
